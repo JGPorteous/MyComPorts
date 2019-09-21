4385b1548ba9b3802c0b4c581aae4e45cb8dfdac
Justin Porteous <justin@porteous.co.za>
2019-09-21 11:49:14 +0200

    Initial Commit

---
+++ MyComPorts.cs
@@ -0,0 +1,102 @@
+﻿using MyComPorts.Properties;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyComPorts
+{
+    public class MyComPorts : ApplicationContext
+    {
+        private NotifyIcon trayIcon;
+        private Timer timer = new Timer();
+        private long lastUpdated = 0;
+        private ContextMenu contextMenu;
+
+        public MyComPorts()
+        {
+            timer.Interval = 500;
+            timer.Tick += Tick;
+            timer.Start();
+
+            contextMenu = new ContextMenu(new MenuItem[] {
+                    new MenuItem("Exit", Exit)
+                });
+
+            trayIcon = new NotifyIcon()
+            {
+                Icon = Resources.AppIcon,
+                ContextMenu = contextMenu,
+                Visible = true
+            };
+
+            PortManager.Refresh();
+        }
+
+        private void Tick(object sender, EventArgs e)
+        {
+            if (lastUpdated == 0 || PortManager.LastUpdated > lastUpdated)  {
+                ShowNotification();
+                BuildMenu();
+
+                lastUpdated = PortManager.LastUpdated;
+                System.Diagnostics.Debug.WriteLine("Updated Com Ports!");
+            }
+        }
+
+        private void ShowNotification()
+        {
+            if (PortManager.LastUpdateAction != string.Empty)
+            {
+                trayIcon.BalloonTipTitle = "Device Status Changed";
+                trayIcon.BalloonTipText = PortManager.LastUpdateAction;
+                trayIcon.BalloonTipIcon = ToolTipIcon.Info;
+
+                trayIcon.ShowBalloonTip(1000);
+            }
+        }
+
+        private void BuildMenu()
+        {
+            trayIcon.ContextMenu.MenuItems.Clear();
+            MenuItem menuItemNoComPorts = new MenuItem("No COM Ports found!");
+            menuItemNoComPorts.Enabled = false;
+
+            contextMenu = new ContextMenu(new MenuItem[0]);
+
+            PortManager.Refresh();
+
+            if (PortManager.SerialPortNames.Count() > 0)
+                for (int i = 0; i < PortManager.SerialPortNames.Count(); i++)
+                    contextMenu.MenuItems.Add(new MenuItem(PortManager.SerialPortFriendlyNames[i].ToString()));
+            else
+                contextMenu.MenuItems.Add(menuItemNoComPorts);
+
+            contextMenu.MenuItems.Add(new MenuItem("-"));
+            contextMenu.MenuItems.Add(new MenuItem("Refresh", Refresh));
+            contextMenu.MenuItems.Add(new MenuItem("Exit", Exit));
+
+
+            trayIcon.ContextMenu = contextMenu;
+        }
+
+        void Refresh(object sender, EventArgs e)
+        {
+            PortManager.Refresh();
+
+            BuildMenu();
+        }
+
+        void Exit(object sender, EventArgs e)
+        {
+            trayIcon.Visible = false;
+            trayIcon.Icon = null;
+
+            Application.Exit();
+        }
+
+    }
+}
---
+++ PortManager.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyComPorts
+{
+    public static class PortManager
+    {
+        public static string[] SerialPortNames { get; set; }
+        public static string[] SerialPortFriendlyNames { get; set; }
+        public static long LastUpdated { get; set; } = 0;
+
+        private static string lastUpdateAction = string.Empty;
+
+        public static string LastUpdateAction {
+            get {
+                string returnValue = lastUpdateAction;
+                lastUpdateAction = string.Empty;
+                return returnValue;
+            }
+            set { lastUpdateAction = value; }
+        }
+
+        static PortManager()
+        {
+            MonitorUSBPorts();
+            Refresh();
+        }
+
+        public static void Refresh()
+        {
+            LastUpdated++;
+
+            LoadPortNames();
+            LoadFriendlyNames();
+        }
+
+        private static void LoadPortNames()
+        {
+            SerialPortNames = System.IO.Ports.SerialPort.GetPortNames();
+        }
+
+        private static void LoadFriendlyNames()
+        {
+            using (var searcher = new ManagementObjectSearcher("SELECT * FROM Win32_PnPEntity WHERE Caption like '%(COM%'"))
+            {
+                var portnames = SerialPort.GetPortNames();
+                var ports = searcher.Get().Cast<ManagementBaseObject>().ToList().Select(p => p["Caption"].ToString());
+
+                SerialPortFriendlyNames = portnames.Select(n => n + " - " + ports.FirstOrDefault(s => s.Contains(n))).ToArray();
+            }
+        }
+
+        private static void MonitorUSBPorts()
+        {
+            WqlEventQuery insertQuery = new WqlEventQuery("SELECT * FROM __InstanceCreationEvent WITHIN 2 WHERE TargetInstance ISA 'Win32_USBHub'");
+
+            ManagementEventWatcher insertWatcher = new ManagementEventWatcher(insertQuery);
+            insertWatcher.EventArrived += new EventArrivedEventHandler(DeviceInsertedEvent);
+            insertWatcher.Start();
+
+            WqlEventQuery removeQuery = new WqlEventQuery("SELECT * FROM __InstanceDeletionEvent WITHIN 2 WHERE TargetInstance ISA 'Win32_USBHub'");
+            ManagementEventWatcher removeWatcher = new ManagementEventWatcher(removeQuery);
+            removeWatcher.EventArrived += new EventArrivedEventHandler(DeviceRemovedEvent);
+            removeWatcher.Start();
+        }
+
+        private static void DeviceInsertedEvent(object sender, EventArrivedEventArgs e)
+        {
+            LastUpdateAction = "Device Inserted!";
+            Refresh();
+        }
+
+        private static void DeviceRemovedEvent(object sender, EventArrivedEventArgs e)
+        {
+            LastUpdateAction = "Device Removed";
+            Refresh();
+        }
+
+    }
+}
